580cd0314add2b8183c890e9fc775b1aaf08c5e8
PrintHolamundo <jdohca@gmail.com>
2022-08-02 13:13:07 -0500

    Servicios

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -2,15 +2,16 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['Toyota', 'Honda', 'Nissan'];
+
+
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return ;
   }
 
   @Get(':id')
   getCarById(@Param('id') id: string) {
-    return this.cars[id]
+    return 
   }
 }
