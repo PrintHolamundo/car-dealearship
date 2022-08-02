9e40abf06194f351b1bb19e1c22d304b3befd4c2
PrintHolamundo <jdohca@gmail.com>
2022-08-02 13:28:36 -0500

    Inyeccion de dependencias

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-
+  constructor(
+    private readonly carsServise: CarsService
+  ) {}
 
   @Get()
   getAllCars() {
-    return ;
+    return this.carsServise.findAll();
   }
 
   @Get(':id')
   getCarById(@Param('id') id: string) {
-    return 
+    return this.carsServise.findOneById(Number(id));
   }
 }
