0bb5036d9f7a8b7284ee246f9408ef3583dff774
PrintHolamundo <jdohca@gmail.com>
2022-08-04 09:39:25 -0500

    Actualizar el listado de carros

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -27,10 +28,10 @@ export class CarsController {
 
   @Patch(':id')
   updateCar(
-    @Param('id',ParseIntPipe) id: number,
-    @Body() body: any
+    @Param('id',ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto
   ) {
-    return body
+    return this.carsServise.update(id, updateCarDto);
   }
 
   @Delete(':id')
---
+++ src/cars/dto/update-car.dto.ts
@@ -0,0 +1,19 @@
+import { IsOptional, IsString, IsUUID } from "class-validator";
+
+
+export class UpdateCarDto {
+
+    @IsString()
+    @IsUUID()
+    @IsOptional()
+    readonly id?: string;
+
+    @IsString({message :`The brand must be a string`})
+    @IsOptional()
+    readonly brand?: string;
+
+    @IsString()
+    @IsOptional()
+    readonly model?: string;
+
+}
