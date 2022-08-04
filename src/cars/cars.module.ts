4fa02d2dd6f1e3b7e274c7099f5e0a5ff73cf376
PrintHolamundo <jdohca@gmail.com>
2022-08-04 14:26:07 -0500

    Inyectar servicios en otros servicios

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BrandsService } from 'src/brands/brands.service';
 import { CarsController } from './cars.controller';
 import { CarsService } from './cars.service';
 
 @Module({
   controllers: [CarsController],
   providers: [CarsService],
+  exports: [CarsService],
 })
 export class CarsModule {}
